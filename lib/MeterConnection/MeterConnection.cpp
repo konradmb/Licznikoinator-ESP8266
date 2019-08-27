49147729bee8d69774d6cd70bd92b067676148ef
Konrad <konradmb@o2.pl>
2019-08-28 01:30:04 +0200

    Read message from meter

--- lib/MeterConnection/MeterConnection.cpp
+++ lib/MeterConnection/MeterConnection.cpp
@@ -2,15 +2,59 @@
 
 String meterRead()
 {
-    const int pin = 5;
-    SoftwareSerial swSer1(2, pin, true);
-    // swSer1.enableRx(false);
-    // swSer1.enableTx(true);
-    swSer1.begin(300, SWSERIAL_7N1);
-    swSer1.write("/?!\r\n");
-    delay(1000);
-    swSer1.write(0x06); //ACK
-    swSer1.write("000\r\n");
-    swSer1.end();
-    return "Ok";
+    String meterMessage = String();
+    const int txPin = 5;
+    const int rxPin = 4;
+
+    digitalWrite(txPin, 0);
+
+    ConfigurableSoftwareSerial swSerWrite(rxPin, txPin, true);
+    swSerWrite.begin(300, 1, 'E', 7);
+    swSerWrite.enableRx(false);
+    Serial.print("swSerWrite.baudRate: ");
+    Serial.println(swSerWrite.baudRate());
+    swSerWrite.write("/?!\r\n");
+
+    SoftwareSerial swSerReadSlow(rxPin, txPin, true);
+    swSerReadSlow.begin(300, SWSERIAL_7N1);
+    char readChar;
+    do
+    {
+        ESP.wdtFeed();
+        while (swSerReadSlow.available() > 0)
+        {
+            readChar = swSerReadSlow.read();
+            meterMessage += readChar;
+            Serial.write(readChar);
+        }
+    } while (readChar != (char)'\n');
+    swSerReadSlow.end();
+
+    swSerWrite.write(0x06); //ACK
+    swSerWrite.write("020\r\n");
+
+
+    swSerWrite.end();
+
+    SoftwareSerial swSerReadFast(rxPin, txPin, true);
+    swSerReadFast.begin(1200, SWSERIAL_7N1);
+
+    // delay(300); //needed by protocol
+
+    do
+    {
+        ESP.wdtFeed();
+        while (swSerReadFast.available() > 0)
+        {
+            readChar = swSerReadFast.read();
+            meterMessage += readChar;
+            Serial.write(readChar);
+        }
+    } while (readChar != (char)0x03); //ETX
+
+    meterMessage.remove(meterMessage.length() - 1);
+    meterMessage.remove(0, 1);
+
+    digitalWrite(txPin, 0);
+    return meterMessage;
 }
